36d61d46458926be9aac04c039399ee734c44d98
Sebast2711 <oillosebast@gmail.com>
2021-02-16 15:31:03 +0100

    Ajout de commentaires

--- js/event.js
+++ js/event.js
@@ -1,4 +1,3 @@
-
 let brandlogo = document.querySelector('#brand-logo');
 let brandp = document.querySelector ('#brand-p');
 let navHamburger = document.querySelector('#hamburger-icon');
@@ -18,7 +17,17 @@ navHamburger.addEventListener('click', ()=>{
 
 
 
+let form = document.querySelector("#form-select-pizzas");
+let inputNom = document.querySelector('#champs-nom');
+let inputPrix1 = document.querySelector ('#champs-prix-1');
+let inputPrix2 = document.querySelector ('#champs-prix-2');
+let inputIngredientsSelect = document.querySelector ('#champs-select-ingredients');
+let inputIngredientsSelected = document.querySelector ('#champs-selected-ingredients');
 
+
+
+
+// Calcul le minimum du prix d'une pizza proposée sur le site
 function min (){
     let min ;
     if (tabPizza.length > 0)
@@ -34,6 +43,7 @@ function min (){
     return min;
 }
 
+// Calcul le maximum du prix d'une pizza proposée sur le site
 function max (){
     let max;
     if (tabPizza.length>0)
@@ -45,13 +55,16 @@ function max (){
         if (tabPizza[i].prix > max){
             max = tabPizza[i].prix;
         }
-    }
-            
+    }            
     return max;    
 }
 
+// Initialise les champs reservés au prix avec le minimum et la maximum des prix possible pour une pizza 
+inputPrix1.value = min();
+inputPrix2.value = max();
 
 
+// Recuperer les ingredients de toutes le pizzas
 function getIngredientsFromAllPizzas (){
     let ingredientsFromAllPizzas = [];
 
@@ -59,67 +72,54 @@ function getIngredientsFromAllPizzas (){
         for (let j = 0; j < pizza.ingredients.length; j++){
             let ingredient = pizza.ingredients[j];
             ingredient = ingredient.toLowerCase();
+            // Si l'ingredient n'est pas déjà présent dans le tableau alors on l'ajoute
             if (!ingredientsFromAllPizzas.includes(ingredient)){
                 ingredientsFromAllPizzas[ingredientsFromAllPizzas.length] = ingredient;
             }
         } 
     });
-    
     return ingredientsFromAllPizzas;
 }
 
-/* Ajoute la liste des ingredients au formulaire dans le select */
 
-/* Pour ameliorer la fonction: */
-/*faire des groupes (viandes, fromages, sauces, etc ...)  */
 
+/* Ajoute la liste des ingredients au select dans le formulaire */
+
+/* A FAIRE : groupes (viandes, fromages, sauces, etc ...)  */
 function setIngredientsInSelectInput (){
     let ingredientsFromAllPizzas = getIngredientsFromAllPizzas ();
     let inputIngredientsSelect = document.querySelector('#champs-select-ingredients');
     
     for (let i = 0; i < ingredientsFromAllPizzas.length; i ++){
         let ingredient = ingredientsFromAllPizzas[i];
+        // Ajoute une option au select
         inputIngredientsSelect.innerHTML += `<option value="${ingredient}"> ${ingredient}  </option>`
     }
 }
-
 setIngredientsInSelectInput ();
 
-let inputIngredientsSelect = document.querySelector ('#champs-select-ingredients');
-let inputIngredientsSelected = document.querySelector ('#champs-selected-ingredients');
+
+
+
+// Tableau des ingredients présents dans la liste des ingredients selectionnés
 let tabIngredients = [];
-
-let inputNom = document.querySelector('#champs-nom');
-let inputPrix1 = document.querySelector ('#champs-prix-1');
-let inputPrix2 = document.querySelector ('#champs-prix-2');
-let inputVegetarienne = document.querySelector('#champs-vegetarienne');
-let inputDisponible = document.querySelector ('#champs-disponible');
-
-
-
-inputPrix1.value = min();
-inputPrix2.value = max();
-
-
-
-
 
 /* Ajoute une liste d'ingredients choisis en dessous du select */
 inputIngredientsSelect.addEventListener ('change', () => {
-
-    // console.log (inputIngredientsSelect.value);
 
 
     /* Test si l'ingredient n'est pas deja dans la liste */
     if ((!tabIngredients.includes(inputIngredientsSelect.value ) || tabIngredients.length == 0)){
         tabIngredients.push (inputIngredientsSelect.value);
 
+        // Création d'un emplacement pour afficher l'ingrédient selectionné
         let divIngredientsSelected = document.createElement ('div');
         divIngredientsSelected.classList.add ('div-ingredient');
         let nomIngredientSelected = document.createElement ('p');
         nomIngredientSelected.classList.add ('nom-ingredient');
         nomIngredientSelected.innerHTML = inputIngredientsSelect.value;
 
+        // Création de la croix pour supprimer l'ingrédient
         let croix = document.createElement ('p');
         croix.classList.add ('croix-list');
         croix.innerHTML = 'X';
@@ -134,19 +134,31 @@ inputIngredientsSelect.addEventListener ('change', () => {
 
         function clickedCroix (e){
             let ingredientASupprimerStr = e.target.parentElement.children[0].innerText;
+            // Suppression de l'ingrédient du tableau tabIngredient
+            // Suppression de la div contenant l'ingrédient et la croix
             if (tabIngredients.includes(ingredientASupprimerStr)){
                 tabIngredients.splice(tabIngredients.indexOf(ingredientASupprimerStr), 1);
                 e.target.parentElement.remove();
-                console.log (tabIngredients);
             }
         }
+        // Ecoute du click sur la croix pour toutes les croix disponibles
         for (i = 0; i < tabCroix.length; i++){  
             tabCroix[i].addEventListener ('click', clickedCroix);
         }
 
     }
+    // Si l'ingrédient est déjà dans la liste
     else {
-        console.log("L'ingredient est déjà dans la liste des ingrédients");
+        // Création d'un élément pour prevenir l'utilisateur
+        let pWarning = document.createElement ('p');
+        pWarning.classList.add('warning');
+        pWarning.innerHTML = 'Ingredient deja selectionné';
+        inputIngredientsSelected.appendChild(pWarning);
+
+        // Suppresion de cet élement après 3 secondes
+        setTimeout(() => {
+            pWarning.remove();
+        }, 3000); 
     }
 
     /* Remise a 'Choisissez un ingredient' */
@@ -159,19 +171,14 @@ inputIngredientsSelect.addEventListener ('change', () => {
 
 
 
-let form = document.querySelector("#form-select-pizzas");
+// A AMELIORER
 
-
+// Afficher Uniquement les pizzas de tabPizza concernées par les filtres
 
 form.addEventListener ('submit', (e)=>{
     e.preventDefault();
-    // let inputIngredientsSelect = document.querySelector ('#champs-select-ingredients');
-    // let inputIngredientsSelected = document.querySelector ('#champs-selected-ingredients');
-
-
-    // Afficher Uniquement les pizzas de tabPizza concernées par les filtres
-
     let pizzaAAfficher = [];
+
     tabPizza.forEach(pizza => {
 
         // Créé un tableau avec les ingredients en minuscules
@@ -207,8 +214,6 @@ form.addEventListener ('submit', (e)=>{
 
 /* Affiche les pizzas sous forme de cartes avec une image, un nom, un prix et une liste des ingredients */
 function afficherPizza (tableauPizza){
-    console.log (tableauPizza);
-
     let pizzaDOM =  document.querySelector('.resultatPizza');
     if (tableauPizza.length > 0){
 
@@ -241,7 +246,5 @@ function afficherPizza (tableauPizza){
             divCard.appendChild(pizzaPrix);
             divCard.appendChild(pizzaParagraphe);
         });
-
     }   
-
 }
